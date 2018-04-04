1e64980466daa6dd04abb24980a42068bde84531
Bleve <wsbaihekong@gmail.com>
2018-04-04 21:03:00 +1000

    4.04
    build up big map, first  map and npc

--- KnightPrinciple/app/src/main/java/com/example/bleve/knightprinciple/home.java
+++ KnightPrinciple/app/src/main/java/com/example/bleve/knightprinciple/home.java
@@ -1,5 +1,6 @@
 package com.example.bleve.knightprinciple;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,6 +33,14 @@ public class home extends AppCompatActivity {
         // define value
         final ImageView c1 = (ImageView) findViewById(R.id.c1);
         final TextView text = (TextView) findViewById(R.id.text);
+
+        Button btn_map = (Button) findViewById(R.id.map);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(home.this, first_map.class));
+            }
+        });
 
 
         // animation of character
@@ -83,6 +93,8 @@ public class home extends AppCompatActivity {
     public void onBackPressed() {
 
     }
+
+    // turn the visibility of textview to invisible
     public boolean onTouchEvent(MotionEvent e) {
         final TextView text = (TextView) findViewById(R.id.text);
         text.setVisibility(View.INVISIBLE);
