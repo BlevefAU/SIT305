b3a1f56c79b3f2a0c265e48010ca1adde36e1214
Bleve <wsbaihekong@gmail.com>
2018-05-10 23:39:14 +1000

    5.10

---
+++ kp/app/src/main/java/com/example/bleve/knightprinciple/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.bleve.knightprinciple;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread mythread = new Thread(){
+            @Override
+            public void run (){
+                try{
+                    sleep(5000);
+                    Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(it);
+                    finish();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        mythread.start();
+    }
+}
